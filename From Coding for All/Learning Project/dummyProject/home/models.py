cc3731f5cc07119abf21cc040e244d49cbc852f8
Raviagr0143 <raviagrahari2023cse@sece.ac.in>
2024-10-18 19:01:50 +0530

    learnt basics in django

--- From Coding for All/Learning Project/dummyProject/home/models.py
+++ From Coding for All/Learning Project/dummyProject/home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Friends(models.Model):
+    name  = models.CharField(max_length=50)
+    age = models.IntegerField()
+    address = models.TextField()
+    email = models.EmailField()
+    
+class Cars(models.Model):
+    name = models.CharField(max_length=50)
+    speed = models.IntegerField()
+
+ 
